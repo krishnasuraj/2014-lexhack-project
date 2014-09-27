32e36d4cb9c264a972ea308faa1e83aa096f9716
jangulani <krishnasuraj@ssrinivasan-mbp.ci.lexington.ma.us>
2014-09-27 11:48:32 -0400

    2nd

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // $('#student').mouseenter(function(){
+    //     $('#student').fadeTo('fast', 1);
+    // });
+    // $('#student').mouseleave(function(){
+    //     $('#student').fadeTo('fast',0.5);
+    // });
+    $('#student').click(function(){
+        window.location="student.html"
+    });
+});
